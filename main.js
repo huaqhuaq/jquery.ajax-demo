030f234a7d2b0dd4bf5cff9d4f7bc67d52106a04
huaqhuaq <rabbit2131101@163.com>
2018-12-09 22:03:42 +0800

    jquery封装ajax

--- main.js
+++ main.js
@@ -5,48 +5,42 @@ window.jQuery = function(nodeOrSelector){
   return nodes
 }
 window.$ = window.jQuery
-
-window.Promise = function(fn){
-  // ...
-  return {
-    then: function(){}
+ window.jQuery.ajax = function({url, method, body, success, fail, headers}){
+  let request = new XMLHttpRequest()
+  request.open(method, url) // 配置request
+  for(let key in headers) {
+    let value = headers[key]
+    request.setRequestHeader(key, value)
   }
-}
-
-window.jQuery.ajax = function({url, method, body, headers}){
-  return new Promise(function(resolve, reject){
-    let request = new XMLHttpRequest()
-    request.open(method, url) // 配置request
-    for(let key in headers) {
-      let value = headers[key]
-      request.setRequestHeader(key, value)
-    }
-    request.onreadystatechange = ()=>{
-      if(request.readyState === 4){
-        if(request.status >= 200 && request.status < 300){
-          resolve.call(undefined, request.responseText)
-        }else if(request.status >= 400){
-          reject.call(undefined, request)
-        }
+  request.onreadystatechange = ()=>{
+    if(request.readyState === 4){
+      if(request.status >= 200 && request.status < 300){
+        success.call(undefined, request.responseText)
+      }else if(request.status >= 400){
+        fail.call(undefined, request)
       }
     }
-    request.send(body)
-  })
+  }
+  request.send(body)
 }
-
-myButton.addEventListener('click', (e)=>{
-  let promise = window.jQuery.ajax({
-    url: '/xxx',
+ function f1(responseText){}
+function f2(responseText){}
+ myButton.addEventListener('click', (e)=>{
+  window.jQuery.ajax({
+    url: '/frank',
     method: 'get',
     headers: {
       'content-type':'application/x-www-form-urlencoded',
       'frank': '18'
+    },
+    success: (x)=>{
+      f1.call(undefined,x)
+      f2.call(undefined,x)
+    },
+    fail: (x)=>{
+      console.log(x)
+      console.log(x.status)
+      console.log(x.responseText)
     }
   })
-
-  promise.then(
-    (text)=>{console.log(text)},
-    (request)=>{console.log(request)}
-  )
-
-})
+}) 
